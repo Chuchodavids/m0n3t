2e991d686f3498e50eedd3b6b64a38d43bd73956
jdtorrenegraneiu <jdtorrenegra@neiu.edu>
2022-02-24 19:09:44 -0600

    more changes

--- main.tf
+++ main.tf
@@ -32,24 +32,24 @@ resource "google_container_cluster" "primary" {
   name                  = "my-gke-cluster"
   location              = "us-central1"
   enable_shielded_nodes = true
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
+  network               = google_compute_network.vpc.name
+  subnetwork            = google_compute_subnetwork.subnet.name
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block = "CHANGE_ME"
+      cidr_block = "69.230.65.40/32"
     }
   }
   cluster_autoscaling {
     enabled = true
     resource_limits {
       resource_type = "memory"
-      minimum       = 2
-      maximum       = 8
+      minimum       = 8
+      maximum       = 68
     }
     resource_limits {
       resource_type = "cpu"
       minimum       = 2
-      maximum       = 4
+      maximum       = 64
     }
   }
   remove_default_node_pool = true
@@ -57,18 +57,19 @@ resource "google_container_cluster" "primary" {
 }
 # GKE pool
 resource "google_container_node_pool" "non_default_pool" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
+  initial_node_count = 1
+  name     = "${google_container_cluster.primary.name}-node-pool"
+  location = var.region
+  cluster  = google_container_cluster.primary.name
   autoscaling {
-    min_node_count = 1
-    max_node_count = 10
+    min_node_count = 2
+    max_node_count = 3
   }
 
   node_config {
+    disk_size_gb = 30
     preemptible  = true
-    machine_type = "n1-standard-1"
-
+    machine_type = "e2-standard-2"
     service_account = google_service_account.default.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
