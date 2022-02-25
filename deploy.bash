c1e09cade878a8bfea18041117128c730b9530d9
jdtorrenegraneiu <jdtorrenegra@neiu.edu>
2022-02-24 20:43:50 -0600

    final commit -- hope so

--- deploy.bash
+++ deploy.bash
@@ -13,11 +13,13 @@ echo "Retrieving gke credentials"
 gcloud container clusters get-credentials my-gke-cluster --region us-central1 --project protean-torus-342301
 echo "Running istio kustomize"
 kustomize build istio/ | kubectl apply -f -
+kustomize build istio/ | kubectl apply -f - # yes, this is repeated on purpose
+while [ "$?" -ne 0 ] ; do !!; done
 echo "Running argocd kustomize"
 while [ "$?" -ne 0 ] ; do !!; done
 kustomize build argocd/ | kubectl apply -f -
-# echo "Running sample-app kustomize"
-# while [ "$?" -ne 0 ] ; do !!; done
-# kustomize build sample-app/ | kubectl apply -f -
+echo "adding apps to argocd"
+while [ "$?" -ne 0 ] ; do !!; done
+kustomize build managed-apps/ | kubectl apply -f -
 
 echo "Done"
